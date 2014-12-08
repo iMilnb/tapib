65dc3463419dca1a1f9c38fe24bbd71c3826ab6c
Sébastien Marie <semarie@users.noreply.github.com>
2014-12-08 15:25:24 +0100

    use a precomputer dict for constants
    - build at start-time a dict for CATEGORIES and ORDERS

--- tapib.py
+++ tapib.py
@@ -15,20 +15,28 @@ api = Api(app)
 
 t = TPB(tpburl)
 
-# main subject and default value
-constants = {
-            'cat': [CATEGORIES, CATEGORIES.ALL],
-            'order': [ORDERS, ORDERS.SEEDERS.DES]
-        }
+# convert a twolevel constants to flatten dict
+#  { 'other:other' => CATEGORIES.OTHER.OTHER }
+def make_twolevel_constants(obj, result={}):
+    # main level
+    main = [e for e in dir(obj) if not e.startswith('_')]
 
-def abort_on_mistake(var, filtr):
+    # sub level, except 'ALL'
+    for main_entry in [e for e in main if not e == 'ALL']:
+        sub_obj = getattr(obj, main_entry)
+        for sub_entry in [e for e in dir(sub_obj) if not e.startswith('_')]:
+            key   = '{0}:{1}'.format(main_entry.lower(), sub_entry.lower())
+            result[key] = getattr(sub_obj, sub_entry)
+
+    return result
+
+categories = make_twolevel_constants(CATEGORIES, {'all': CATEGORIES.ALL})
+orders = make_twolevel_constants(ORDERS)
+
+def abort_on_mistake(a_dict, a_value):
     try:
-        if not ':' in filtr:
-            # return default value
-            return constants[var][1]
-        k, v = filtr.split(':')
-        return getattr(getattr(constants[var][0], k.upper()), v.upper())
-    except AttributeError:
+        return a_dict[a_value.lower()]
+    except KeyError:
         abort(404, message='unkown value')
 
 def append_torrent(r):
@@ -42,23 +50,13 @@ def append_torrent(r):
            }
 
 class Categories(Resource):
-    categories = ['all']
     def get(self):
-        if len(self.categories) > 1:
-            return self.categories
-        # main categories
-        cats = [a for a in dir(CATEGORIES) if not a.startswith('__')]
-        # sub categories except ALL
-        for c in [s for s in cats if not s == 'ALL']:
-            subcat = getattr(CATEGORIES, c)
-            for sub in [s for s in dir(subcat) if not s.startswith('__')]:
-                self.categories.append('{0}:{1}'.format(c.lower(), sub.lower()))
-        return self.categories
+        return categories.keys()
 
 class Top(Resource):
     def get(self, category):
         top = []
-        cat = abort_on_mistake('cat', category)
+        cat = abort_on_mistake(categories, category)
         for r in t.top().category(cat):
             top.append(append_torrent(r))
         return top
@@ -67,7 +65,7 @@ class Search(Resource):
     def get(self, category='all', sstr=None, sort=ORDERS.SEEDERS.DES):
         search = []
 
-        cat = abort_on_mistake('cat', category)
+        cat = abort_on_mistake(categories, category)
 
         for r in t.search(sstr).category(cat).order(sort):
             search.append(append_torrent(r))
@@ -75,7 +73,7 @@ class Search(Resource):
 
 class SortSearch(Resource):
     def get(self, category='all', sstr=None, sort='seeders:des'):
-        s = abort_on_mistake('order', sort)
+        s = abort_on_mistake(orders, sort)
         print(s)
         return Search.get(self, category=category, sstr=sstr, sort=s)
 
